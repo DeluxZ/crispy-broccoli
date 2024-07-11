c329b2f80169ffd381625d7a312580fca763f932
Harrie Essing <harrie.essing@kpn.com>
2024-07-11 09:30:13 +0200

    feat: remove all fetch code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-"use client";
+// "use client";
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import useSWR from "swr";
+// import { useEffect, useState } from "react";
+// import useSWR from "swr";
 
 // async function getData() {
 //   const res = await fetch(`api/Locations?name=harrie`)
@@ -13,16 +13,16 @@ import useSWR from "swr";
 //   return res.json();
 // }
 
-const fetcher = (url:any) => fetch(url).then((res) => res.json());
+// const fetcher = (url:any) => fetch(url).then((res) => res.json());
 
 export default function Home() {
   
-  const { data, error, isLoading } = useSWR(`api/Locations?name=harrie`, fetcher);
+  // const { data, error, isLoading } = useSWR(`api/Locations?name=harrie`, fetcher);
 
   return (
     
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      { data }
+      {/* { data } */}
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
